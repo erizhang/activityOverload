f492325c26e078626935e94c519553a871907a40
erizhang <zhang.lyuan@gmail.com>
2015-04-13 11:17:18 +0800

    add the show pages

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -12,5 +12,31 @@ module.exports = {
         //req.session.flash = {};
     },
 
+     create: function(req, res, next) {
+
+            User.create(req.params.all(), function userCreated (err, user){
+                    if (err){
+                            //console.log(err);
+                            req.session.flash = {
+                                    err: err
+                            }
+                            return res.redirect('/user/new');
+                    }
+                    //res.json(user);
+                    //req.session.flash = {};
+                    res.redirect('/user/show/' + user.id);
+            });
+    },
+
+	show: function(req, res, next) {
+        User.findOne(req.param('id'), function foundUser(err, user){
+                if (err) return next(err);
+                if (!user) return next();
+                res.view({
+                        user: user
+                });
+        });
+    },
+
 };
 
