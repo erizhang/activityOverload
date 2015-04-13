df2a18062be1064361cd4633ec036296e7fdd6aa
erizhang <zhang.lyuan@gmail.com>
2015-04-13 11:24:48 +0800

    add index page

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -14,29 +14,40 @@ module.exports = {
 
      create: function(req, res, next) {
 
-            User.create(req.params.all(), function userCreated (err, user){
-                    if (err){
-                            //console.log(err);
-                            req.session.flash = {
-                                    err: err
-                            }
-                            return res.redirect('/user/new');
-                    }
-                    //res.json(user);
-                    //req.session.flash = {};
-                    res.redirect('/user/show/' + user.id);
-            });
+        User.create(req.params.all(), function userCreated (err, user){
+            if (err){
+                //console.log(err);
+                req.session.flash = {
+                    err: err
+                }
+                return res.redirect('/user/new');
+            }
+            //res.json(user);
+            //req.session.flash = {};
+            res.redirect('/user/show/' + user.id);
+        });
     },
 
 	show: function(req, res, next) {
         User.findOne(req.param('id'), function foundUser(err, user){
-                if (err) return next(err);
-                if (!user) return next();
-                res.view({
-                        user: user
-                });
+            if (err) return next(err);
+            if (!user) return next();
+            res.view({
+                    user: user
+            });
         });
     },
+
+    index: function(req, res, next) {
+        User.find(function foundUser(err, users){
+            if (err) return next(err);
+
+            res.view({
+                users: users
+            });
+        });
+    },
+
 
 };
 
