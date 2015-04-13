c942a19d9fa435738a7691cae7469b112089473b
erizhang <zhang.lyuan@gmail.com>
2015-04-13 13:41:28 +0800

    load the js and layout minor changes

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -30,6 +30,8 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
+  'linker/js/jquery.js',
+  'linker/**/*.js',
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js'
